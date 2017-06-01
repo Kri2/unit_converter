70b376e2d3c3d5aacfd23c3060e7a78c282427a0
Krzysztof Turlej <kriz@Krzysztofs-MacBook-Pro.local>
2017-06-01 18:16:21 +0200

    form now rewrites input to output

--- units-converter-application/src/main/java/pl/traza/UnitsConverterDAO.java
+++ units-converter-application/src/main/java/pl/traza/UnitsConverterDAO.java
@@ -1,5 +1,13 @@
 package pl.traza;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class UnitsConverterDAO {
-
+	private Float temperatureInCelsius = 0.0F;
+	public void setTemperatureInCelsius(Float temperatureInCelsius){
+		this.temperatureInCelsius=temperatureInCelsius;
+	}
+	public Float getTemperatureInCelsius(){
+		return this.temperatureInCelsius;
+	}
 }
--- units-converter-webapp/src/main/java/pl/traza/UnitsConverterController.java
+++ units-converter-webapp/src/main/java/pl/traza/UnitsConverterController.java
@@ -13,6 +13,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class UnitsConverterController {
+	@Autowired
+	UnitsConverterDAO unitsConverterDAO;  //czy to wystarczy? czy gdzieś indziej to się jeszcze inicializuje?
+	
 	@RequestMapping(value = "/converter", method = RequestMethod.GET)
 	public String showUnitConverterForm(Model model){
 		UnitConverter unitConverter = new UnitConverter();
@@ -22,7 +25,17 @@ public class UnitsConverterController {
 	
 	@RequestMapping(value = "/converter", method = RequestMethod.POST)
 	public String updateUnitConverterForm(@ModelAttribute("unitConverterForm") UnitConverter unitConverter, BindingResult result, Model model){
-		return "converter";
+		//trochę dziwne ze moge tu uzywac ten unitConverter przeciez on jest lokalny w funkcji wyzej, moze mozna go przeniesc tu?
+		//dalem tutaj i co się okazało, on jest przekazany przecież w argumencie, pojawiła sie info o zdublowanej zmiennej, czyli juz tu jest
+		//tylko skad jest w argumencie? nie ważne na razie
+		unitsConverterDAO.setTemperatureInCelsius( unitConverter.getTemperatureC());
+		//return "converter";
+		return "redirect:/resultpage";
 	}
 	
+	@RequestMapping(value = "/resultpage")
+	public String showResult(Model model){
+		model.addAttribute("conversionResult", unitsConverterDAO.getTemperatureInCelsius());//no tu mi brakuje źródła danych DAO
+		return "resultpage";
+	}
 }
